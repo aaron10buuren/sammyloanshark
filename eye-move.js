63f3bceb94dd6c3cff8a804704d21dab24451f17
aaron10buuren <aaron.tenbuuren@gmail.com>
2015-12-04 14:37:28 -0500

    added Favs - removed vars
    was recalling the var i had previously set in the JS - removed the
    resetting ones. Added Favicons for 16, 32, and 64px

--- eye-move.js
+++ eye-move.js
@@ -1,16 +1,19 @@
 var box, boxCenter;
 
 $(document).ready(function() {
-  box=$(".box");
-  boxCenter=[box.offset().left+box.width()/2, box.offset().top+box.height()/2];
-});
+	
+	box=$(".box");
+	boxCenter=[box.offset().left+box.width()/2, box.offset().top+box.height()/2];
+
+})
+
+$(document).mousemove(function(e){    
+            
+	var angle = Math.atan2(e.pageX- boxCenter[0],- (e.pageY- boxCenter[1]) )*(180/Math.PI);	    
     
-        
-var angle = Math.atan2(e.pageX- boxCenter[0],- (e.pageY- boxCenter[1]) )*(180/Math.PI);	    
-    
-  box.css({ "-webkit-transform": 'rotate(' + angle + 'deg)'});    
-  box.css({ '-moz-transform': 'rotate(' + angle + 'deg)'});
-  box.css({ 'transform': 'rotate(' + angle + 'deg)'});
+    box.css({ "-webkit-transform": 'rotate(' + angle + 'deg)'});    
+    box.css({ '-moz-transform': 'rotate(' + angle + 'deg)'});
+    box.css({ 'transform': 'rotate(' + angle + 'deg)'});
     
 });
 
